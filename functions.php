49337763502c74e5fd01cf3966ee6ed4cf55b5a5
Rene Pihlak <pihlakre@greeny.cs.tlu.ee>
2015-10-14 15:06:17 +0300

    Fixed Delete

--- functions.php
+++ functions.php
@@ -3,11 +3,61 @@
 	require_once("../konfig_global.php");
 	$database = "if13_rene_p";
 	
-	function $getCarData(){
+	function getCarData(){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL");
+		$stmt->bind_result($id_from_db, $user_id_from_db, $number_plate_from_db, $color_from_db);
+		$stmt->execute();
+		
+		$array = array();
 		
 		
+		while($stmt->fetch()){
 		
+		//loon objekti
+		$car = new StdClass();
+		$car->id = $id_from_db;
+		$car->number_plate = $number_plate_from_db;
+		$car->color = $color_from_db;
+		$car->user_id = $user_id_from_db;
+		
+		array_push($array, $car);
+		//echo "<pre>";
+		//var_dump($array);
+		//echo "</pre>";
+		
+			
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		return $array;
+	}
+	
+	
+	
+	function deleteCar($id_to_be_deleted){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
+		$stmt->bind_param("i", $id_to_be_deleted);
+		
+		if($stmt->execute()){
+			// sai edukalt kustutatud
+			header("Location: table.php");
+			
+		}
+		
+		$stmt->close();
+		$mysqli->close();
 		
 	}
+	
 
 ?>
+
+
--- table.php
+++ table.php
@@ -1,5 +1,45 @@
 <?php
-
-kek
-
+	
+	// table.php
+	require_once("functions.php");
+	
+	if(isset($_GET["delete"])){
+		//saadan kaasa id mida kustutada
+		
+		deleteCar($_GET["delete"]);
+		
+	}
+	
+	$car_list = getCarData();
 ?>
+
+<table border=1 >
+	<tr>
+		<th>ID</th>
+		<th>Auto nr</th>
+		<th>V2rv</th>
+		<th>User ID</th>
+		
+	</tr>
+	
+	<?php
+	
+		for($i = 0; $i < count($car_list); $i++){
+			echo "<tr>";
+			
+			echo "<td>".$car_list[$i]->id."</td>";
+			echo "<td>".$car_list[$i]->number_plate."</td>";
+			echo "<td>".$car_list[$i]->color."</td>";
+			echo "<td>".$car_list[$i]->user_id."</td>";
+			
+			echo "<td><a href='?delete=".$car_list[$i]->id."'>X</a></td>";
+			
+			
+			echo "</tr>";
+			
+		}
+	
+	
+?>
+</table>
+
